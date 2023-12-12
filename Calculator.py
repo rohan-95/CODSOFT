fdd2da130aeaa844d586030b10aa66f26666918e
rohan-95 <65495692+rohan-95@users.noreply.github.com>
2023-12-12 10:49:45 +0530

    Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -39,6 +39,9 @@ while True:
 
         elif choice == '4':
             print(num1, "/", num2, "=", divide(num1, num2))
+            
+        next_calculation = input("Let's do next calculation? (yes/no): ")
+        if next_calculation == "no":    
           break
     else:
         print("Invalid Input")
